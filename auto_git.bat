34ab58f785edffc6e9f440a6ff4ceb4a1650e8b9
Foo Wei Shien <fooshien541@gmail.com>
2025-05-23 16:20:17 +0800

    *commit at: 2025-23-05 16:20

--- auto_git.bat
+++ auto_git.bat
@@ -1,5 +1,5 @@
-@REM USE FOR GIT AUTO COMMIT AND AUTO PUSH TO GITHUB REPOSITORY
-@REM THIS CODE IS USING ABANG TO GENERATE FOR EASY WAYS TO MAKE COMMITMENT
+:: USE FOR GIT AUTO COMMIT AND AUTO PUSH TO GITHUB REPOSITORY
+:: THIS CODE IS USING ABANG TO GENERATE FOR EASY WAYS TO MAKE COMMITMENT
 
 @echo off
 :: get current time and date：2025-05-23 14:35:12
@@ -18,7 +18,18 @@ set TIMESTAMP=%YYYY%-%MM%-%DD% %HH%:%Min%
 set COMMIT_MSG=Auto commit on %date% %time%
 if not "%~1"=="%COMMIT_MSG%" set COMMIT_MSG=%~1 *commit at: %TIMESTAMP%
 
+:: 添加所有变更并提交
 git add .
 git commit -m "%COMMIT_MSG%"
-git remote add origin https://github.com/FooAloysius/PDI_Project_2025.git
+
+:: 检查是否已经设置 origin
+git remote get-url origin >nul 2>&1
+if errorlevel 1 (
+    echo Adding origin...
+    git remote add origin https://github.com/FooAloysius/PDI_Project_2025.git
+) else (
+    echo Origin already exists, skipping add.
+)
+
+:: 推送到 master 分支（根据需要改为 main）
 git push origin master
