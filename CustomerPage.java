157180b565164f4e2cc1ff7ce43cba7639e3072e
Foo Wei Shien <fooshien541@gmail.com>
2025-06-11 12:04:52 +0800

    *commit at: 2025-11-06 12:04

--- CustomerPage.java
+++ CustomerPage.java
@@ -12,8 +12,11 @@ import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 public class CustomerPage {
+  JPanel mainPanel;
+  JPanel modal;
   public JPanel customerList () {
     List<PetOwner> customers= new Data().getCustomers();
 
@@ -50,9 +53,31 @@ public class CustomerPage {
       trashBinButton.addActionListener((actionEvent) -> {
         System.out.println("Delete " + customer.getPetOwnerName());
       });
+
+      // Image Icon for modifier
+      ImageIcon modifierIcon = new ImageIcon(((new ImageIcon("./icons/pencil_icon_200px.png")).getImage()).getScaledInstance(30, 30, java.awt.Image.SCALE_SMOOTH));
+      ImageIcon modifierIconHover = new ImageIcon(((new ImageIcon("./icons/pencil_icon_hover_200px.png")).getImage()).getScaledInstance(30, 30, java.awt.Image.SCALE_SMOOTH)); // hover icon
+      // button to pet panel
+      JButton modifierButton = new JButton("Modify details", modifierIcon);
+      modifierButton.setContentAreaFilled(false); // transparent background
+      modifierButton.setBorderPainted(false); // set border to none
+      modifierButton.setCursor(new Cursor(Cursor.HAND_CURSOR)); // when mouse hover, set cursor to pointer
+      modifierButton.setFocusable(false);
+      modifierButton.setVerticalTextPosition(JButton.BOTTOM);
+      modifierButton.setHorizontalTextPosition(JButton.CENTER);
+      modifierButton.setRolloverIcon(modifierIconHover);
+      modifierButton.addActionListener((actionEvent) -> {
+        modal = customerModify(customer);
+        System.out.println("modify for");
+        mainPanel.removeAll();
+        mainPanel.add(modal);
+        mainPanel.revalidate();
+        mainPanel.repaint();
+      });
       
       listElement.add(nameLabel);
       listElement.add(trashBinButton);
+      listElement.add(modifierButton);
       listPanel.add(Box.createVerticalStrut(5));
       listPanel.add(listElement);
       
@@ -61,7 +86,41 @@ public class CustomerPage {
     return listPanel;
   }
 
+  public JPanel customerModify (PetOwner customer) {
+    JPanel customerModifyModal = new JPanel();
+
+    // customer id
+    JPanel idGroup = new JPanel();
+    JLabel idLabel = new JLabel("Customer ID: ");
+    JLabel idInput = new JLabel(customer.getPetOwnerID());
+    idInput.setPreferredSize(new Dimension(250,40));
+    idGroup.add(idLabel);
+    idGroup.add(idInput);
+
+    // customer name
+    JPanel nameGroup = new JPanel();
+    JLabel nameLabel = new JLabel("Customer Name: ");
+    JTextField nameInput = new JTextField(customer.getPetOwnerName());
+    nameInput.setPreferredSize(new Dimension(250,40));
+    nameGroup.add(nameLabel);
+    nameGroup.add(nameInput);
+
+    // customer contact
+    JPanel contactGroup = new JPanel();
+    JLabel contactLabel = new JLabel("Customer contact: ");
+    JTextField contactInput = new JTextField(Integer.toString(customer.getPetOwnerContact()));
+    contactInput.setPreferredSize(new Dimension(250,40));
+    contactGroup.add(contactLabel);
+    contactGroup.add(contactInput);
+
+    customerModifyModal.add(idGroup);
+    customerModifyModal.add(nameGroup);
+    customerModifyModal.add(contactGroup);
+    return customerModifyModal;
+  }
+
   public void gui (JPanel panel) {
+    mainPanel = panel;
     panel.removeAll();
 
     JPanel customerList = customerList();
